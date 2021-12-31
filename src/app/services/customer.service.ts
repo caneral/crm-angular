c7d5014313ffcb2cfc98cd988094f52c0f57207a
Caner AL <caneral5858@gmail.com>
2022-01-01 00:35:20 +0300

    Table added and delete operation created 🔨

---
+++ src/app/services/customer.service.ts
@@ -0,0 +1,17 @@
+import { Customer } from './../@core/types/customer';
+import { Injectable } from '@angular/core';
+import { CustomersDb } from 'src/fakedb/customersDb';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomerService {
+
+  constructor() { }
+
+  remove(customer:Customer){
+    let item = CustomersDb.find(c => c.id === customer.id);
+    CustomersDb.splice(CustomersDb.indexOf(item!),1)
+  }
+
+}
